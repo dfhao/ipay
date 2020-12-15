1ddaa525000264bedf7720777c4e3dc72892be42
dfhao <dfhao12@gmail.com>
2020-12-15 14:39:29 +0800

    构建springboot项目

---
+++ src/main/java/com/dfhao/ipay/IpayApplication.java
@@ -0,0 +1,13 @@
+package com.dfhao.ipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpayApplication.class, args);
+    }
+
+}
